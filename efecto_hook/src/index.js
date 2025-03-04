7ba7c178d3de4f60a504d0b438a613f895525e14
acastem15 <85703757+acastem15@users.noreply.github.com>
2025-03-04 09:47:42 -0500

    efecto hook

---
+++ efecto_hook/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ImageViewer from './components/ImageViewer';
+import Mascotas from './components/mascotas';
+import Mascota from './components/mascota';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <Container className="mt-3">
+ <Row>
+
+   <Mascotas />
+ </Row>
+ </Container>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
